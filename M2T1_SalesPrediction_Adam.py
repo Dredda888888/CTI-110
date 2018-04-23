79bfe72cc72164639f52e2ea4afad900379496e1
Dredda888888 <31837454+Dredda888888@users.noreply.github.com>
2018-04-22 22:01:46 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Adam.py
@@ -0,0 +1,9 @@
+# CTI-110 
+# M2T1 - Sales Prediction 
+# Christopher Adam
+# 4/22/2018
+#
+print ("Please Input Your Projected Sales ?")
+Sales_Total = float(input("Sales_= "))
+Profit = (Sales_Total * .23)
+print (Profit)
